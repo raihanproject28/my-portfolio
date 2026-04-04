41ebb346f4ee68ea3777dc77f9a9f14a8bea75c5
Raihan <raihanproject28@gmail.comcd>
2026-04-05 01:07:08 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold mb-4">
+        Halo, saya Raihan 👋
+      </h1>
+
+      <p className="text-lg text-gray-300">
+        Ini website portofolio pertama saya 🚀
+      </p>
+    </main>
+  );
+}
